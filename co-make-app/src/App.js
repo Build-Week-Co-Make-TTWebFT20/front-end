06fd7e47cd15451b7bd07e069041cb9778c5c32e
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-02-01 21:19:35 -0700

    created base for add issue form

--- co-make-app/src/App.js
+++ co-make-app/src/App.js
@@ -1,16 +1,17 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import PrivateRoute from './components/PrivateRoute';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <Router>
-      {/* link the login component here */}
-      <Route exact path='/'/>
-      {/* link the issues landing page here */}
-      <PrivateRoute exact path='/issues'/>
+        {/* link the login component here */}
+        <Route exact path='/' />
+        {/* link the issues landing page here */}
+        <PrivateRoute exact path='/issues' component={Dashboard} />
       </Router>
       <h1>HEY HEY HAY</h1>
     </div>
